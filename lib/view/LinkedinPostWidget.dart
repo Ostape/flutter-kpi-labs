c7c1cd839581020d5d18ca1e709eebe54e46b077
Orest Shemeliuk <orestshemelyuk@gmail.com>
2020-12-07 12:23:12 +0200

    create comments fields

--- lib/view/LinkedinPostWidget.dart
+++ lib/view/LinkedinPostWidget.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/gestures.dart';
+  import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/model/Post.dart';
 import 'package:flutter_app/view/BottomActionPostWidget.dart';
@@ -67,65 +67,86 @@ class CommentsWidget extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         clipBehavior: Clip.antiAlias,
-        child: Column(
-          children: [
-            LinkedinPostWidget(
-              post: post,
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: [Text("Comments")],
+        child: Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            children: [
+              LinkedinPostWidget(
+                post: post,
               ),
-            ),
-            Container(
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [Text("Comments")],
+                ),
+              ),
+              CommentWidget(post: post),
+              CommentWidget(post: post),
+              CommentWidget(post: post),
+              CommentWidget(post: post),
+            ],
+          ),
+        ),
+      ),
+      bottomSheet: Text("SOME TEXT"),
+    );
+  }
+}
+
+class CommentWidget extends StatelessWidget {
+  const CommentWidget({
+    Key key,
+    @required this.post,
+  }) : super(key: key);
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      width: double.infinity,
+      color: Colors.red,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: 35,
+            height: 35,
+            margin: EdgeInsets.only(right: 10),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(post.userAvatar),
+                )),
+          ),
+          Expanded(
+            child: Container(
               padding: EdgeInsets.all(10),
-              width: double.infinity,
-              color: Colors.red,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.only(
+                  topRight: const Radius.circular(7),
+                  bottomRight: const Radius.circular(7),
+                  bottomLeft: const Radius.circular(7),
+                )
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Text("Orest Shemeliuk"),
+                  Text("Software Engineer"),
+                  Text("2w"),
                   Container(
-                    width: 35,
-                    height: 35,
-                    margin: EdgeInsets.only(right: 10),
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: NetworkImage(post.userAvatar),
-                        )),
-                  ),
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        borderRadius: BorderRadius.only(
-                          topRight: const Radius.circular(7),
-                          bottomRight: const Radius.circular(7),
-                          bottomLeft: const Radius.circular(7),
-                        )
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Orest Shemeliuk"),
-                          Text("Software Engineer"),
-                          Text("2w"),
-                          Container(
-                            margin: EdgeInsets.only(top: 10),
-                            child: Text("I am interest in it"),
-                          ),
-                        ],
-                      ),
-                    ),
+                    margin: EdgeInsets.only(top: 10),
+                    child: Text("I am interest in it"),
                   ),
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
